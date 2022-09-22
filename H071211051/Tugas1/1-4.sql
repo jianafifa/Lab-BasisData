3eee7c365c0328e20dbbf9030f46a98240cbf01d
febift <febifian23@gmail.com>
2022-09-22 15:32:27 +0800

    kumpul tugas 1

---
+++ H071211051/Tugas1/1-4.sql
@@ -0,0 +1,19 @@
+-- CREATE DATABASE Office
+-- CREATE TABLE offices(
+-- officeCode VARCHAR (50) NOT NULL,
+-- city VARCHAR (50) NOT NULL,
+-- phone VARCHAR (50) NOT NULL,
+-- adressLine1 VARCHAR (50) NOT NULL,
+-- adressLine2 VARCHAR (50) NOT NULL,
+-- state VARCHAR (50) NOT NULL,
+-- country VARCHAR (50) NOT NULL,
+-- PRIMARY KEY (officeCode)
+-- )
+
+-- DESCRIBE offices
+
+-- ALTER TABLE offices
+-- MODIFY phone INT(20) NOT NULL
+
+ALTER TABLE offices
+DROP adressLine2
