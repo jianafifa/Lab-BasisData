f04c613f5f72d8a41f7a5f6d88cbb89a994847cc
aimlana <salim.yaklu@gmail.com>
2022-09-27 15:07:26 +0800

    kumpul tugas 2

--- H071211019/Materi_02/tugas2.sql
+++ H071211019/Materi_02/tugas2.sql
@@ -1,6 +1,20 @@
--- SELECT * FROM offices WHERE city = 'San Francisco'
--- SELECT * FROM orderdetails WHERE quantityOrdered > 70 ORDER BY orderLineNumber 
--- SELECT DISTINCT productLine FROM products
--- SELECT customerNumber noKonsumen, customerName namaKonsumer FROM customers WHERE customerNumber >= 100 && customerNumber <= 150
+-- No. 1
+SELECT * FROM offices WHERE city = 'San Francisco';
 
-SELECT * FROM customers WHERE country != 'USA' AND creditLimit = 0 ORDER BY customerName LIMIT 9,19
+-- No. 2
+SELECT * FROM orderdetails WHERE quantityOrdered > 70 ORDER BY orderLineNumber ;
+
+-- No. 3
+SELECT DISTINCT productLine FROM products;
+
+-- No. 4
+SELECT customerNumber noKonsumen, customerName namaKonsumer FROM customers WHERE customerNumber BETWEEN 100 AND 150;
+
+-- No. 5
+SELECT * FROM customers WHERE country != 'USA' AND creditLimit = 0 ORDER BY customerName LIMIT 9,10;
+
+
+-- Challenge
+SELECT * FROM orderdetails WHERE productCode NOT LIKE 'S12%' ORDER BY quantityOrdered DESC, priceEach DESC;
+
+SELECT * FROM customers WHERE state IS NULL;
